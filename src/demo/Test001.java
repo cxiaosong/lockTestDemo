ee6dc15c96daea463a406e9ce7d3bfa6af70abea
cxiaosong <cxiaosong@foxmail.com>
2023-06-05 10:39:16 +0800

    refactoring to readlock

--- src/demo/Test001.java
+++ src/demo/Test001.java
@@ -3,9 +3,12 @@ package demo;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test001 {
-	public synchronized void sayhello() {
+	static ReentrantReadWriteLock lock=new ReentrantReadWriteLock();
+	public  void sayhello() {
 		// TODO Auto-generated method stub
+		lock.writeLock().lock();
 		System.out.println("hello");
+		lock.writeLock().unlock();
 	}
 
 }
