5380ed92bee5da15b37cfcfeb00ca8cfc14caa26
cxiaosong <cxiaosong@foxmail.com>
2023-06-05 10:38:00 +0800

    delete syn

--- src/demo/Test001.java
+++ src/demo/Test001.java
@@ -3,7 +3,7 @@ package demo;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test001 {
-	public synchronized void sayhello() {
+	public void sayhello() {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 	}
